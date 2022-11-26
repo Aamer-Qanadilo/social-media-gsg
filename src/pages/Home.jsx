d2d54d9da43904724a0071f0b51d799c1af40b70
Aamer-Qanadilo <aamer_qanadilo@hotmail.com>
2022-11-26 09:52:08 +0200

    Added files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React, { useContext } from "react";
+import AddPost from "../components/AddPost";
+import Posts from "../components/Posts";
 import { PostsContext } from "../components/PostsContext";
+import Tags from "../components/Tags";
 
 const Home = () => {
   const { posts } = useContext(PostsContext);
   console.log(posts);
 
-  return <div>Home</div>;
+  return (
+    <React.Fragment>
+      <Tags />
+      <AddPost />
+      <Posts />
+    </React.Fragment>
+  );
 };
 
 export default Home;
